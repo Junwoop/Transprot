6f6ba81f55f2aac6af3fcc6b2d5ea85da02e1739
Junwoop <101860126+Junwoop@users.noreply.github.com>
2022-05-02 10:20:59 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='Transprot', #followed Tutorial by Eu4ng
+
+version='1.0.0.2',
+
+description='Translate DNA string into Protein, based on codon table',
+
+author='Junwoop',
+
+author_email='louispark9019@gmail.com',
+
+url='https://github.com/Junwoop',
+
+license='MIT',
+
+py_modules=['Transprot'],
+
+python_requires='>=3',
+
+install_requires=[],
+
+packages=['Transprot']
+
+      )
+
+
+
+
